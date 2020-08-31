125a353c34cb88642bcc64f6a2c88db34bf8c65b
behrooz2011 <bmmm1370@gmail.com>
2020-08-31 06:53:46 -0400

    Bash Scripting

---
+++ redirect/redirect.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+data = input("Enter sth, This is my keyboard : ")
+print(" This is the key you typed: ",data)
+raise ValueError("and this is the raise ValueError oooojjjjjj")
---
+++ redirect/simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+print(" hoooo!")
+data = input("This is my keyboard : ")
+print(" This is the key you typed: ",data)
+raise ValueError("and this is the raise ValueError")
