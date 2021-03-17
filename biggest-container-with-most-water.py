f7114a096d435f612cd75adebb605386fb371a33
behrooz2011 <bmmm1370@gmail.com>
2021-03-17 16:59:57 -0400

    the biggest container with most water

---
+++ biggest-container-with-most-water.py
@@ -0,0 +1,12 @@
+def maxContainer(x):
+  arr=[]
+  for index,value in enumerate(x):
+    for elem in range(index+1,len(x)):
+      z=min(value,x[elem]) * (elem-index)
+      arr.append(z)
+  return max(arr)
+  
+print(maxContainer([1,9,3,5,10]))
+print(maxContainer([1,8,6,2,5,4,8,3,7]))
+print(maxContainer([1,1]))
+print(maxContainer([4,3,2,1,4]))
